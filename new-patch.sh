125ecc83fb4947f9a96424ec6d678f92a45758e4
Jebaitedneko <jebaitedneko@gmail.com>
2023-08-17 15:32:22 +0000

    cleanup

--- new-patch.sh
+++ new-patch.sh
@@ -25,17 +25,6 @@ __patch:
     push   %rbp
     mov    %rsp,%rbp
     mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
-    mov    %eax, %eax
     pop    %rbp
     ret
 .size __patch, .-__patch
@@ -52,19 +41,14 @@ EOF
 ${PFX}as -c patch.S -o patch.o && rm patch.S
 ${PFX}readelf -a patch.o > re-patch.txt
 
-# echo "==== patch.o objdump start ===="
-# ${PFX}objdump -d patch.o
-# echo "==== patch.o objdump end ===="
+echo "==== patch.o objdump start ===="
+${PFX}objdump -d patch.o
+echo "==== patch.o objdump end ===="
 
 ${PFX}objcopy --dump-section .text=patch.text patch.o && rm patch.o
 PATCH_TEXT_SZ=$(printf %x $(stat -c '%s' patch.text) | sed 's/^0*//;s/^/0x/')
 echo "==== PATCH_TEXT_SZ: $PATCH_TEXT_SZ ===="
-cat patch.text >> target.text
-# ${PFX}objcopy --add-section .patch=patch.text \
-#               --set-section-flags .patch=code,alloc,load,readonly,data,contents \
-#               --change-section-address .patch=0x1770  target target_patch && mv target_patch target && rm target.text
-# ${PFX}objcopy --add-symbol __patch=".patch:0,function" target target_patch && mv target_patch target
-rm patch.text
+cat patch.text >> target.text && rm patch.text
 TARGET_TEXT_SZ_PATCHED=0x$(printf %x $(stat -c '%s' target.text))
 echo "==== TARGET_TEXT_SZ_PATCHED: $TARGET_TEXT_SZ_PATCHED ===="
 NEXT_ADDR=$(printf %x $(($PATCH_TEXT_SZ)) | sed 's/^0*//;s/^/0x/')
@@ -72,20 +56,19 @@ echo "NEXT_ADDR: $NEXT_ADDR"
 
 ${PFX}objcopy --update-section .text=target.text target target_patch && mv target_patch target && rm target.text
 ${PFX}objcopy --add-symbol __patch=".text:${TARGET_TEXT_SZ_ORIG},function" target target_patch && mv target_patch target
-# ${PFX}objcopy --change-section-address .fini+$PATCH_TEXT_SZ target target_patch && mv target_patch target
+
+# patch the section following .text
 S_ADDR=$(readelf -a target | grep ".text  " -A1 | tr -d '\n' | sed "s/  \+/\n/g" | sed -n 4p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
 S_OFF=$(readelf -a target | grep ".text  " -A1 | tr -d '\n' | sed "s/  \+/\n/g" | sed -n 6p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
 S_ALIGN=0x16
 RELOC=$(printf %x $((S_ADDR+S_OFF+16)) | sed 's/^0*//;s/^/0x/')
-PATCH_SECTION=$(${PFX}readelf -a target | grep ".fini  " | grep -oE "\[.*\]" | sed "s/\[//g;s/\]//g" | tr -d '\n')
+PATCH_SECTION=$(${PFX}readelf -a target | grep ".fini  " | grep -oE "\[.*\]" | sed "s/\[//g;s/\]//g" | tr -d '\n') # change section here (plt for arm)
 ./modelf target --section $PATCH_SECTION --addr $RELOC
 mv modelf-out.elf target
 
-# printf %x $((0x1060+0x142+16)) # text addr + text size + text align
-
 ${PFX}readelf -a target > re-target_pst.txt
-# diff -ur re-target_pre.txt re-target_pst.txt
+diff -ur re-target_pre.txt re-target_pst.txt
 
-# echo "==== target objdump start ===="
-# ${PFX}objdump -d target
-# echo "==== target objdump end ===="
+echo "==== target objdump start ===="
+${PFX}objdump -d target
+echo "==== target objdump end ===="
