8525ff1ce72ecf748951e06fe1b4e116be27d602
Jebaitedneko <jebaitedneko@gmail.com>
2023-08-17 15:30:11 +0000

    this-works

--- new-patch.sh
+++ new-patch.sh
@@ -1,4 +1,7 @@
+PFX=aarch64-linux-gnu-
 PFX=
+
+${PFX}gcc modelf.c -o modelf
 
 cat << EOF > target.c
 #include <stdio.h>
@@ -8,39 +11,81 @@ EOF
 # #include <stdio.h>
 # int main(){return 1;}
 # EOF
-${PFX}gcc target.c -o target -g -Wall
+${PFX}gcc target.c -o target -g -Wall && rm target.c
 ${PFX}objcopy --dump-section .text=target.text target
-TARGET_TEXT_SZ=0x$(printf %x $(stat -c '%s' target.text))
+TARGET_TEXT_SZ_ORIG=$(printf %x $(stat -c '%s' target.text) | sed 's/^0*//;s/^/0x/')
+echo "==== TARGET_TEXT_SZ_ORIG: $TARGET_TEXT_SZ_ORIG ===="
 ${PFX}readelf -a target > re-target_pre.txt
 
 cat << EOF > patch.S
-.text
-.global __patch
+.section .text
+.globl __patch
+.type __patch, @function
 __patch:
-    nop
+    push   %rbp
+    mov    %rsp,%rbp
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    mov    %eax, %eax
+    pop    %rbp
+    ret
+.size __patch, .-__patch
 EOF
 # cat << EOF > patch.S
-# .text
-# .global __patch
+# .section .text
+# .globl __patch
+# .type __patch, @function
 # __patch:
-#     push   %rbp
-#     mov    %rsp,%rbp
-#     pop    %rbp
+#     mov w0, wzr
 #     ret
+# .size __patch, .-__patch
 # EOF
-${PFX}as patch.S -o patch.o
+${PFX}as -c patch.S -o patch.o && rm patch.S
 ${PFX}readelf -a patch.o > re-patch.txt
 
-${PFX}objdump -d patch.o
+# echo "==== patch.o objdump start ===="
+# ${PFX}objdump -d patch.o
+# echo "==== patch.o objdump end ===="
 
-${PFX}objcopy --dump-section .text=patch.text patch.o
+${PFX}objcopy --dump-section .text=patch.text patch.o && rm patch.o
+PATCH_TEXT_SZ=$(printf %x $(stat -c '%s' patch.text) | sed 's/^0*//;s/^/0x/')
+echo "==== PATCH_TEXT_SZ: $PATCH_TEXT_SZ ===="
 cat patch.text >> target.text
+# ${PFX}objcopy --add-section .patch=patch.text \
+#               --set-section-flags .patch=code,alloc,load,readonly,data,contents \
+#               --change-section-address .patch=0x1770  target target_patch && mv target_patch target && rm target.text
+# ${PFX}objcopy --add-symbol __patch=".patch:0,function" target target_patch && mv target_patch target
+rm patch.text
+TARGET_TEXT_SZ_PATCHED=0x$(printf %x $(stat -c '%s' target.text))
+echo "==== TARGET_TEXT_SZ_PATCHED: $TARGET_TEXT_SZ_PATCHED ===="
+NEXT_ADDR=$(printf %x $(($PATCH_TEXT_SZ)) | sed 's/^0*//;s/^/0x/')
+echo "NEXT_ADDR: $NEXT_ADDR"
 
-${PFX}objcopy --update-section .text=target.text target target_patch && mv target_patch target
-${PFX}objcopy --add-symbol __patch=".text:${TARGET_TEXT_SZ},function,global" target target_patch && mv target_patch target
-# ${PFX}objcopy --adjust-section-vma .fini=0x1185 target target_patch && mv target_patch target
+${PFX}objcopy --update-section .text=target.text target target_patch && mv target_patch target && rm target.text
+${PFX}objcopy --add-symbol __patch=".text:${TARGET_TEXT_SZ_ORIG},function" target target_patch && mv target_patch target
+# ${PFX}objcopy --change-section-address .fini+$PATCH_TEXT_SZ target target_patch && mv target_patch target
+S_ADDR=$(readelf -a target | grep ".text  " -A1 | tr -d '\n' | sed "s/  \+/\n/g" | sed -n 4p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
+S_OFF=$(readelf -a target | grep ".text  " -A1 | tr -d '\n' | sed "s/  \+/\n/g" | sed -n 6p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
+S_ALIGN=0x16
+RELOC=$(printf %x $((S_ADDR+S_OFF+16)) | sed 's/^0*//;s/^/0x/')
+PATCH_SECTION=$(${PFX}readelf -a target | grep ".fini  " | grep -oE "\[.*\]" | sed "s/\[//g;s/\]//g" | tr -d '\n')
+./modelf target --section $PATCH_SECTION --addr $RELOC
+mv modelf-out.elf target
+
+# printf %x $((0x1060+0x142+16)) # text addr + text size + text align
 
 ${PFX}readelf -a target > re-target_pst.txt
-diff -ur re-target_pre.txt re-target_pst.txt
+# diff -ur re-target_pre.txt re-target_pst.txt
 
-${PFX}objdump -d target
+# echo "==== target objdump start ===="
+# ${PFX}objdump -d target
+# echo "==== target objdump end ===="
