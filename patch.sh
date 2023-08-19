c5f739614f7aaee94721d6ed0fcadfb03ca1a029
Jebaitedneko <jebaitedneko@gmail.com>
2023-08-19 18:28:45 +0000

    implement multiple section shifts

--- patch.sh
+++ patch.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 PFX=aarch64-linux-gnu-
-PFX=
+# PFX=
 
 # Prepare Target Binary
 cat << EOF > target.c
@@ -22,11 +24,11 @@ cat << EOF > patch.S
 .globl __patch
 .type __patch, @function
 __patch:
-    push   %rbp
-    mov    %rsp,%rbp
-    xor    %eax, %eax
-    inc    %eax
-    pop    %rbp
+    mov w0, #1
+    mov w0, #1
+    mov w0, #1
+    mov w0, #1
+    mov w0, #1
     ret
 .size __patch, .-__patch
 EOF
@@ -50,40 +52,82 @@ cat patch.text >> target.text && rm patch.text
 TARGET_TEXT_SZ_PATCHED=0x$(printf %x $(stat -c '%s' target.text))
 echo "==== TARGET_TEXT_SZ_PATCHED: $TARGET_TEXT_SZ_PATCHED ===="
 
-# Compute address of section after .text (this will get resized)
-S_ADDR=$(${PFX}readelf -a target | grep ".text  " -A3 | tail -n2 | tr -d '\n' | sed "s/  \+/\n/g"  | sed -n 5p | tr -d '\n' | sed 's/^0*//' | tac -rs .. | echo "$(tr -d '\n')")
+# [PRE-RUN] Update .text section with new .text data
+${PFX}objcopy --update-section .text=target.text target target.temp &> objcopy-out.txt && rm target.temp
+S_CNT=$(cat objcopy-out.txt | wc -l)
+S_DATA=$(cat objcopy-out.txt | cut -f3 -d: | sed "s/.*section //g;s/lma //g;s/adjusted to //g;s/\n/ /g;s/ /\n/g")
+rm objcopy-out.txt
+
+i=0
+SECTIONS=()
+ADDR_OLD=()
+ADDR_NEW=()
+while [ $i -lt $S_CNT ]; do
+    i=$((i+1))
+    SECTIONS+=( $(echo -e "$S_DATA" | head -n $((3*$i)) | tail -n3 | head -n1 | tail -n1) )
+
+    ADDR_OLD_TMP=$(echo -e "$S_DATA" | head -n $((3*$i)) | tail -n3 | head -n2 | tail -n1)
+    ADDR_OLD_TMP_SZ=$(echo $ADDR_OLD_TMP | tr -d '\n' | wc -c)
+    if [ $(($ADDR_OLD_TMP_SZ%2)) -ne 0 ]; then
+        ADDR_OLD_TMP=$( echo $ADDR_OLD_TMP | sed "s/0x/0x0/g" )
+    fi
+    ADDR_OLD+=( $ADDR_OLD_TMP )
+
+    ADDR_NEW_TMP=$(echo -e "$S_DATA" | head -n $((3*$i)) | tail -n3 | head -n3 | tail -n1)
+    ADDR_NEW_TMP_SZ=$(echo $ADDR_NEW_TMP | tr -d '\n' | wc -c)
+    if [ $(($ADDR_NEW_TMP_SZ%2)) -ne 0 ]; then
+        ADDR_NEW_TMP=$( echo $ADDR_NEW_TMP | sed "s/0x/0x0/g" )
+    fi
+    ADDR_NEW+=( $ADDR_NEW_TMP )
+done
+echo ${SECTIONS[@]}
+echo ${ADDR_OLD[@]}
+echo ${ADDR_NEW[@]}
 
 # Compute .dynamic section address and offset
 DYN_ADDR=$(${PFX}readelf -a target | grep ".dynamic  " -A1 | tr -d '\n' | sed "s/  \+/\n/g"  | sed -n 5p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
 DYN_SIZE=$(${PFX}readelf -a target | grep ".dynamic  " -A1 | tr -d '\n' | sed "s/  \+/\n/g"  | sed -n 6p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
-DYN_OLDHX=$(xxd -s $(printf %d $DYN_ADDR) -l $(printf %d $DYN_SIZE) -g0 target | grep -oE "[0-9a-f]{32}" | tr -d '\n' | grep -oE ".{,4}$S_ADDR.{,4}")
+xxd -s $(printf %d $DYN_ADDR) -l $(printf %d $DYN_SIZE) -g0 target | grep -oE "[0-9a-f]{32}" | tr -d '\n' > dyn.hex
 
 # Compute .symtab section address and offset
 SYM_ADDR=$(${PFX}readelf -a target | grep ".symtab  " -A1 | tr -d '\n' | sed "s/  \+/\n/g"  | sed -n 5p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
 SYM_SIZE=$(${PFX}readelf -a target | grep ".symtab  " -A1 | tr -d '\n' | sed "s/  \+/\n/g"  | sed -n 6p | tr -d '\n' | sed 's/^0*//;s/^/0x/')
-SYM_OLDHX=$(xxd -s $(printf %d $SYM_ADDR) -l $(printf %d $SYM_SIZE) -g0 target | grep -oE "[0-9a-f]{32}" | tr -d '\n' | grep -oE ".{,4}$S_ADDR.{,4}")
+xxd -s $(printf %d $SYM_ADDR) -l $(printf %d $SYM_SIZE) -g0 target | grep -oE "[0-9a-f]{32}" | tr -d '\n' > sym.hex
 
 # Patch .dynamic and .symtab sections of section following .text
 function patch_symtab_and_dynamic_sections() {
-    DYN_PATCH=$(echo $DYN_OLDHX | sed "s/$S_ADDR/$1/g")
-    DYN_NEWHX=$(echo $DYN_PATCH | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g;s/ /\\\x/g;s/^/\\\x/g;s/\\\x$//g")
-    DYN_OLDHX=$(echo $DYN_OLDHX | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g;s/ /\\\x/g;s/^/\\\x/g;s/\\\x$//g")
-    echo "DYN_PATCH: $DYN_OLDHX|$DYN_NEWHX"
-    sed -i "s|$DYN_OLDHX|$DYN_NEWHX|g" target
 
-    SYM_PATCH=$(echo $SYM_OLDHX | sed "s/$S_ADDR/$1/g")
+    echo "ADDR SHIFT: $1 -> $2 (LE)"
+
+    DYN_OLDHX=$(xxd -s $(printf %d $DYN_ADDR) -l $(printf %d $DYN_SIZE) -g0 target | grep -oE "[0-9a-f]{32}" | tr -d '\n' | grep -oE ".{,4}$1.{,4}")
+    if [ ${#DYN_OLDHX} -gt 2 ]; then
+        DYN_PATCH=$(echo $DYN_OLDHX | sed "s/$1/$2/g")
+        DYN_NEWHX=$(echo $DYN_PATCH | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g;s/ /\\\x/g;s/^/\\\x/g;s/\\\x$//g")
+        DYN_OLDHX=$(echo $DYN_OLDHX | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g;s/ /\\\x/g;s/^/\\\x/g;s/\\\x$//g")
+        echo "DYN_PATCH: $DYN_OLDHX|$DYN_NEWHX"
+        sed -i "s|$DYN_OLDHX|$DYN_NEWHX|g" target
+    fi
+
+    SYM_OLDHX=$(xxd -s $(printf %d $SYM_ADDR) -l $(printf %d $SYM_SIZE) -g0 target | grep -oE "[0-9a-f]{32}" | tr -d '\n' | grep -oE ".{,4}$1.{,4}")
+    SYM_PATCH=$(echo $SYM_OLDHX | sed "s/$1/$2/g")
     SYM_NEWHX=$(echo $SYM_PATCH | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g;s/ /\\\x/g;s/^/\\\x/g;s/\\\x$//g")
     SYM_OLDHX=$(echo $SYM_OLDHX | sed "s/\([0-9a-f][0-9a-f]\)/\1 /g;s/ /\\\x/g;s/^/\\\x/g;s/\\\x$//g")
     echo "SYM_PATCH: $SYM_OLDHX|$SYM_NEWHX"
     sed -i "s|$SYM_OLDHX|$SYM_NEWHX|g" target
+
 }
 
 # Update .text section with new .text data
-${PFX}objcopy --update-section .text=target.text target target_patch &> objcopy-out.txt && mv target_patch target && rm target.text
+${PFX}objcopy --update-section .text=target.text target target_patch && mv target_patch target && rm target.text
 
-# Compute the new addr of section after .text and patch it's symtab and dynamic addresses
-S_NEW_ADDR=$(cat objcopy-out.txt | grep -oE "0x[0-9a-f]+$" | sed "s/0x//g" | tac -rs .. | echo "$(tr -d '\n')")
-patch_symtab_and_dynamic_sections $S_NEW_ADDR && rm objcopy-out.txt
+i=0
+while [ $i -lt $S_CNT ]; do
+    # Compute the new addr of section after .text and patch it's symtab and dynamic addresses
+    S_OLD_ADDR=$(echo ${ADDR_OLD[$i]} | sed "s/0x//g" | tac -rs .. | echo "$(tr -d '\n')")
+    S_NEW_ADDR=$(echo ${ADDR_NEW[$i]} | sed "s/0x//g" | tac -rs .. | echo "$(tr -d '\n')")
+    patch_symtab_and_dynamic_sections $S_OLD_ADDR $S_NEW_ADDR
+    i=$((i+1))
+done
 
 # Add our new function in Patch to the symtab
 ${PFX}objcopy --add-symbol __patch=".text:${TARGET_TEXT_SZ_ORIG},global,function" target target_patch && mv target_patch target
